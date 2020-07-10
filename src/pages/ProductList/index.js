0cf0469956301c70883353a6b37279fe21d7ddd9
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-07-10 16:52:03 -0300

    order message

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { FiShoppingCart, FiEdit2 } from 'react-icons/fi';
 import AllProducts from '../../services/products'
+import AllAdditional from '../../services/additional'
+
 
 import './styles.css';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [additional, setAdditional] = useState([]);
+  const [btnAdditional, setBtnAdditional] = useState(false)
   const [selectedItems, setSelectedItems] = useState([]);
   const [btn, setBtn] = useState(false);
   // eslint-disable-next-line
@@ -14,6 +18,7 @@ function ProductList() {
 
   useEffect(() => {
     setProducts(AllProducts)
+    setAdditional(AllAdditional)
   }, [products])
 
   useEffect(() => {
@@ -26,8 +31,8 @@ function ProductList() {
 
   function addToCart(product) {
     const productObject = {
-      name: product.name,
       quantity: 1,
+      name: product.name,
       add: []
     }
     setSelectedItems([...selectedItems, productObject]);
@@ -39,39 +44,72 @@ function ProductList() {
   }
 
   function handleOrder() {
-    const orderText = selectedItems.toString();
+    const orderText = JSON.stringify(selectedItems);
+    const removeQuotes = orderText.split('"').join('')
+    const removeStart = removeQuotes.split('[{').join('')
+    const removeEnd = removeStart.split('}]').join(', ')
+    const removeProductEnd = removeEnd.split(']},{').join(' ')
+    const removeQuantity = removeProductEnd.split('quantity:').join('')
+    const removeName = removeQuantity.split('name:').join('');
+    // vc tem q checar se o add tem coisa dentro, 
+    //se tiver vc simplesmente apaga tudo
+    //se não vc substiui por com: 
+    let removeAdd = ''
+    if(removeName.includes('[]')){
+      removeAdd = removeName.split('add:[]').join('')
+    }
+    removeAdd = removeName.split('add:[').join('com:') 
+    const addSpace = removeAdd.split(',').join(', ')
+    const addSpaceAfterDD = addSpace.split(':').join(': ')
+
+    console.log(addSpaceAfterDD);
     setOrder(orderText)
-    console.log(orderText);
-    setOrderURL(`https://api.whatsapp.com/send?phone=553194110059&text=Olá, gostaria de ${orderText}`)
+    // setOrderURL(`https://api.whatsapp.com/send?phone=553194110059&text=Olá, gostaria de ${orderText}`)
   }
-  
-  function isSelected(product, product2){
+
+  function isSelected(product, product2) {
     return product.name === product2.name;
   }
-  function upQuantity(product){
-   const newquantity = []
+  function upQuantity(product) {
+    const newquantity = []
     selectedItems.map(element => {
-      if(element.name === product.name)
-      element.quantity++
+      if (element.name === product.name)
+        element.quantity++
 
       newquantity.push(element)
     });
     setSelectedItems(newquantity)
   }
-  function downQuantity(product){
+  function addAdditional(product, additional) {
+    const newAdditional = []
+    selectedItems.map(element => {
+      if (element.name === product.name)
+        element.add.push(additional.name)
+
+      newAdditional.push(element)
+    });
+    setSelectedItems(newAdditional)
+  }
+  function downQuantity(product) {
     const newquantity = []
-     selectedItems.map(element => {
-       if(element.name === product.name)
-       if(element.quantity <= 1){
-         return null
-       }else{
-         element.quantity--
-       }
- 
-       newquantity.push(element)
-     });
-     setSelectedItems(newquantity)
-   }
+    selectedItems.map(element => {
+      if (element.name === product.name)
+        if (element.quantity <= 1) {
+          return null
+        } else {
+          element.quantity--
+        }
+
+      newquantity.push(element)
+    });
+    setSelectedItems(newquantity)
+  }
+  function setAdditionalArea() {
+    setBtnAdditional(true)
+  }
+  function removeAdditionalArea() {
+    setBtnAdditional(false)
+  }
 
   return (
     <div>
@@ -81,25 +119,50 @@ function ProductList() {
           {products.map((product, id) => (
             <>
               <li key={product.id} className="product-item">
-                <img src={require('../../assets/headerlogo4.png')} alt="Foto do produto" />
-                <p className="productInfo">
-                  {product.info}
-                </p>
-                <strong className="productName">
-                  {product.name}
-                </strong>
-                <p className="productPrice">{product.price}</p>
-                {selectedItems.filter(item => isSelected(product, item)).length > 0 ? (
-                  <div>
-                    <button onClick={() => upQuantity(product)}>+</button>
-                    <p>{selectedItems.filter(item => isSelected(product, item))[0].quantity}</p>
-                    <button onClick={() => downQuantity(product)}> - </button>
-                    <button onClick={() => removeFromCart(product)}>x</button>
-                    <button><FiEdit2 size={10} color='#000'/></button>
-                  </div>
-                ) : (
-                    <button onClick={() => addToCart(product)}>adicionar ao carrinho</button>
-                  )}
+                <>
+                  <img src={require('../../assets/headerlogo4.png')} alt="Foto do produto" />
+                  <p className="productInfo">
+                    {product.info}
+                  </p>
+                  <strong className="productName">
+                    {product.name}
+                  </strong>
+                  <p className="productPrice">{product.price}</p>
+
+                  {selectedItems.filter(item => isSelected(product, item)).length > 0 ? (
+                    <div>
+                      <button onClick={() => upQuantity(product)}>+</button>
+                      <p>{selectedItems.filter(item => isSelected(product, item))[0].quantity}</p>
+                      <button onClick={() => downQuantity(product)}> - </button>
+                      <button onClick={() => removeFromCart(product)}>x</button>
+                      {product.type === 'Sanduíches Tradiconais' ||
+                        product.type === 'Sanduíches Especiais' ||
+                        product.type === 'Turma da Picanha' ||
+                        product.type === 'Turma do Greladão' 
+                    ?
+                        <button onClick={setAdditionalArea}><FiEdit2 size={10} color='#000' /></button>
+                        : null}
+
+                    </div>
+                  ) : (
+                      <button onClick={() => addToCart(product)}>adicionar ao carrinho</button>
+                    )}
+                  {product.type === 'Sanduíches Tradiconais' && btnAdditional ||
+                        product.type === 'Sanduíches Especiais' && btnAdditional||
+                        product.type === 'Turma da Picanha' && btnAdditional||
+                        product.type === 'Turma do Greladão' && btnAdditional
+                         ? (
+                    <div>
+                      {AllAdditional.map(additional => (
+                        <button
+                          onClick={() => addAdditional(product, additional)}>
+                          {additional.name}
+                        </button>
+                      ))}
+                      <button onClick={removeAdditionalArea}>confirmar</button>
+                    </div>
+                  ) : null}
+                </>
               </li>
               {/* {products.map(product => {
               const i = 0;
@@ -116,12 +179,12 @@ function ProductList() {
       <>
         {btn === true ?
           <div className="cartBtnContainer">
-            <a href={orderURL}>
+            {/* <a href={orderURL}> */}
               <button className="cartBtn" onClick={handleOrder}>
                 <FiShoppingCart size={15} color="#FFF" className="cartIcon" strokeWidth={3} />
             Finalizar Compra
           </button>
-            </a>
+            {/* </a> */}
           </div>
           : null}
       </>
---
+++ src/services/additional.js
@@ -0,0 +1,82 @@
+const AllAdditional = [
+  {
+    id: 30,
+    type: "Adicionais",
+    name: "Ovo",
+    info: null,
+    price: "R$1,50"
+  }, {
+    id: 31,
+    type: "Adicionais",
+    name: "Batata Palha",
+    info: null,
+    price: "R$1,50"
+  }, {
+    id: 32,
+    type: "Adicionais",
+    name: "Presunto",
+    info: null,
+    price: "R$2,00"
+  }, {
+    id: 33,
+    type: "Adicionais",
+    name: "Milho",
+    info: null,
+    price: "R$1,50"
+  }, {
+    id: 34,
+    type: "Adicionais",
+    name: "Catupiry",
+    info: null,
+    price: "R$3,00"
+  }, {
+    id: 35,
+    type: "Adicionais",
+    name: "Cheddar",
+    info: null,
+    price: "R$3,00"
+  }, {
+    id: 36,
+    type: "Adicionais",
+    name: "Queijo",
+    info: null,
+    price: "R$3,00"
+  }, {
+    id: 37,
+    type: "Adicionais",
+    name: "Bacon",
+    info: null,
+    price: "R$3,00"
+  }, {
+    id: 38,
+    type: "Adicionais",
+    name: "Frango Desfiado",
+    info: null,
+    price: "R$4,50"
+  }, {
+    id: 39,
+    type: "Adicionais",
+    name: "Hamburguer 60g",
+    info: null,
+    price: "R$4,00"
+  }, {
+    id: 40,
+    type: "Adicionais",
+    name: "Hamburguer Picanha 120g",
+    info: null,
+    price: "R$8,00"
+  }, {
+    id: 41,
+    type: "Adicionais",
+    name: "Hambuerguer Picanha 170g",
+    info: null,
+    price: "R$8,00"
+  }, {
+    id: 42,
+    type: "Adicionais",
+    name: "Hamburguer Picanha 200g",
+    info: null,
+    price: "R$9,00"
+  }
+]
+export default AllAdditional;
